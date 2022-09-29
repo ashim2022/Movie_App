7895877cf776fc8930b1f4695b60d235544a6ec5
Ashim <101468606+ashim2022@users.noreply.github.com>
2022-09-29 17:01:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
   "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 const moiveBox = document.querySelector("#movie-box");
-const getMovies = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  showMovies(data);
-};
+// const getMovies = async (url) => {
+//   const response = await fetch(url);
+//   const data = await response.json();
+//   showMovies(data);
+// };
+function getMovies(url){
+  fetch(url).then((data)=>data.json()).then((data)=>{showMovies(data)});
+}
 getMovies(APIURL);
 
 const showMovies = (data) => {
